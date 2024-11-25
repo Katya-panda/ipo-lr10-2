b5961b5a947c4a1fc4f27ec5334336c14e91cf96
Katya Polyak <katya_7002@mail.ru>
2024-11-25 22:14:25 +0300

    Exercise 3

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import date
 import requests
 from bs4 import BeautifulSoup
 import json
@@ -6,6 +7,7 @@ response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
 quotes = soup.find_all('span', class_='text')
 authors = soup.find_all('small', class_='author')
+data = []
 for i, (quote, author) in enumerate(zip(quotes, authors), 1): 
     quote_text = quote.text.strip() 
     author_text = author.text.strip() 
