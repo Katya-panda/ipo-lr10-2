1af8c3a0eaead8888062a9e748b9492275a9a16a
Katya Polyak <katya_7002@mail.ru>
2024-11-25 22:10:20 +0300

    Change main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 url = "https://quotes.toscrape.com/"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
@@ -9,3 +10,5 @@ for i, (quote, author) in enumerate(zip(quotes, authors), 1):
     quote_text = quote.text.strip() 
     author_text = author.text.strip() 
     print(f'{i}. Quote: "{quote_text}"; Author: {author_text};')
+with open('data.json', 'w', encoding='utf-8') as f: json.dump(data, f, ensure_ascii=False, indent=4) 
+print("Данные успешно сохранены в файл data.json.")
